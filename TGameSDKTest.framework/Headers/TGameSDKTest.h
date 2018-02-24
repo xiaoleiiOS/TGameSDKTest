0a7e17e637784f075fc2a2916602051412ed3a0e
wangxiaolei <675407568@qq.com>
2018-02-24 17:27:59 +0800

    frmaework
    将framework上传

---
+++ TGameSDKTest.framework/Headers/TGameSDKTest.h
@@ -0,0 +1,28 @@
+//
+//  TGameSDK.h
+//  TGameSDKTestDemo
+//
+//  Created by TW-C-060 on 2018/2/8.
+//  Copyright © 2018年 xiaolei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/** TGame delegate */
+@protocol TGameDelegate <NSObject>
+
+/** Callback of login success
+ */
+-(void) tLoginSuccessWithType:(NSString *)typeName;
+
+@end
+
+@interface TGameSDKTest : NSObject
+
+@property (nonatomic, weak) id <TGameDelegate> tgameDelegate;
+
++(TGameSDKTest *)instance;
+
+- (void)doLogin;
+
+@end
